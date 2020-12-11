ccbaffb1214c380cbbd5a13fa810bba0e48355cb
makurano <ginsyari1@gmail.com>
2020-12-11 19:35:25 +0900

    Userモデルバリデーション/正規表現追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        with_options presence: true do
+          #半角英数字混合
+          validates :email,               format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+          #@マーク必須の正規表現?
+          validates :encrypted_password
+          validates :nickname
+          #全角での入力「あ〜漢字」正規表現
+          validates :last_name,           format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+          #全角カタカナの正規表現
+          validates :last_name_katakana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+          #全角での入力「あ〜漢字」正規表現
+          validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters." }
+          #全角カタカナの正規表現
+          validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+          validates :birthday
+        end
 end
