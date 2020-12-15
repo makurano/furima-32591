7abb075054c9793c53fce0c1f15a50209ddf6871
makurano <ginsyari1@gmail.com>
2020-12-15 15:55:15 +0900

    Itemテーブルへのアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  
   with_options presence: true do
     # 全角での入力「あ〜漢字」正規表現
     with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' } do
