0ce4cc485a5659b12ca1257b50bd232b5aedc1dd
makurano <ginsyari1@gmail.com>
2020-12-16 10:20:55 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
-  before_action :prevent_edit, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :prevent_edit, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -52,13 +52,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def prevent_edit
-    unless current_user.id == Item.find(params[:id]).user_id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
 end
